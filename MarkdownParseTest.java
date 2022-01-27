0908b770a29c4016726c766d55331b5d7deb6357
ngngo0 <97553043+ngngo0@users.noreply.github.com>
2022-01-26 16:57:47 -0800

    Make tests for all files created

--- MarkdownParseTest.java
+++ MarkdownParseTest.java
@@ -1,9 +1,55 @@
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.ArrayList;
+
 import static org.junit.Assert.*;
 import org.junit.*;
-
+import java.util.*;
 public class MarkdownParseTest {
     @Test
     public void addition() {
         assertEquals(2, 1 + 1);
+    }
+
+    @Test
+    public void firstTest() throws IOException{
+
+        Path fileName = Path.of("test-file.md");
+	    String contents = Files.readString(fileName);
+        ArrayList<String> links = MarkdownParse.getLinks(contents);
+        System.out.println(links);
+        assertEquals(List.of("https://something.com", "some-page.html"),
+        links);
+    }
+
+    @Test
+    public void secondTest() throws IOException{
+
+        Path fileName = Path.of("noParen.md");
+	    String contents = Files.readString(fileName);
+        ArrayList<String> links = MarkdownParse.getLinks(contents);
+        System.out.println(links);
+        assertEquals(List.of(),links);
+    }
+
+    @Test
+    public void thirdTest() throws IOException{
+
+        Path fileName = Path.of("new-file.md");
+	    String contents = Files.readString(fileName);
+        ArrayList<String> links = MarkdownParse.getLinks(contents);
+        System.out.println(links);
+        assertEquals(List.of("https://help.com"),links);
+    }
+
+    @Test
+    public void fourth() throws IOException{
+
+        Path fileName = Path.of("firstline.md");
+	    String contents = Files.readString(fileName);
+        ArrayList<String> links = MarkdownParse.getLinks(contents);
+        System.out.println(links);
+        assertEquals(List.of("https://help.com"),links);
     }
 }
