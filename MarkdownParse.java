4a82bc9f54bccb173f2d8fbeeafa6a2058e48e7e
ngngo0 <97553043+ngngo0@users.noreply.github.com>
2022-02-02 16:45:56 -0800

    failure inducing input
    format.md returns the link when there are spaces between the []and () but we don't want it to. So we fixed it.

--- MarkdownParse.java
+++ MarkdownParse.java
@@ -16,7 +16,7 @@ public class MarkdownParse {
             int nextCloseBracket = markdown.indexOf("]", nextOpenBracket);
             int openParen = markdown.indexOf("(", nextCloseBracket);
             int closeParen = markdown.indexOf(")", openParen);
-            
+
             //difference between Image vs Link & first line fix
             if((nextOpenBracket>0) && markdown.charAt(nextOpenBracket-1)=='!'){
                 currentIndex= closeParen+1;
@@ -56,6 +56,11 @@ public class MarkdownParse {
             return toReturn;
         }
 
+        System.out.println(nextCloseBracket + " " + openParen);
+        if(openParen-nextCloseBracket!=1){
+            currentIndex=closeParen+1;
+            continue;
+        }
         toReturn.add(markdown.substring(openParen + 1, closeParen));
         currentIndex = closeParen + 1;
         
